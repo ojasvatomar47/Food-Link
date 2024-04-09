65e63d3a3ccadfe0101ef8136c2efa447b8a0924
Ojasva Tomar <ojasvatomar47@gmail.com>
2024-04-09 22:22:59 +0530

    Connected to DB

--- server/index.js
+++ server/index.js
@@ -1,11 +1,21 @@
 import express from 'express';
+import connectDB from './db.js';
+import dotenv from 'dotenv';
 
+// Load environment variables
+dotenv.config();
+
+// Initialize Express App
 const app = express();
 const PORT = process.env.PORT || 8800;
 
-// MIDDLEWARES
+// Connect to MongoDB
+connectDB();
 
-// SERVER CONNECTION
+// Middleware
+app.use(express.json());
+
+// Server Connection
 app.listen(PORT, () => {
     console.log(`Server is running on PORT => ${PORT}`);
 });
